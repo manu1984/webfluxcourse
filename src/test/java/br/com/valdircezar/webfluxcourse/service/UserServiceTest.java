56127cc0a752062302cc414bb8326df57ec8e037
manu1984 <manubrms@gmail.com>
2023-07-09 02:24:10 -0400

    add unit test

--- src/test/java/br/com/valdircezar/webfluxcourse/service/UserServiceTest.java
+++ src/test/java/br/com/valdircezar/webfluxcourse/service/UserServiceTest.java
@@ -10,12 +10,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
-import java.util.Objects;
-
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
 
@@ -30,7 +30,7 @@ class UserServiceTest {
     @InjectMocks
     private UserService service;
     @Test
-    void save() {
+    void testSave() {
         UserRequest request = new UserRequest("valdir","valdir@mail.com","123");
         User entity = User.builder().build();
 
@@ -40,11 +40,46 @@ class UserServiceTest {
         Mono<User> result = service.save(request);
 
         StepVerifier.create(result)
-                .expectNextMatches(Objects::nonNull)
+                .expectNextMatches(user -> user.getClass() == User.class)
                 .expectComplete()
                 .verify();
 
         Mockito.verify(reposirory, times(1)) .save(any(User.class));
+
+    }
+
+    @Test
+    void testFindById(){
+        when(reposirory.findById(anyString())).thenReturn(Mono.just(User.builder()
+                        .id("1234")
+                .build()));
+
+        Mono<User> result = service.findById("123");
+
+        StepVerifier.create(result)
+                .expectNextMatches(user -> user.getClass() == User.class
+                && user.getId() == "1234")
+                .expectComplete()
+                .verify();
+
+        Mockito.verify(reposirory, times(1)) .findById(anyString());
+
+    }
+
+    @Test
+    void testFindAll(){
+        when(reposirory.findAll()).thenReturn(Flux.just(User.builder()
+                .id("1234")
+                .build()));
+
+        Flux<User> result = service.finAll();
+
+        StepVerifier.create(result)
+                .expectNextMatches(user -> user.getClass() == User.class)
+                .expectComplete()
+                .verify();
+
+        Mockito.verify(reposirory, times(1)) .findAll();
 
     }
 }
